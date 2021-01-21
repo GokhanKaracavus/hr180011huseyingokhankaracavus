7d9b9bd81dd98d749ee2f7f7b0b538062c00913b
Gökhan Karaçavuş <gokhankaracavus@hotmail.com>
2021-01-21 23:40:50 +0300

    apk

--- app/src/main/java/com/hr180011_huseyin_gokhan_karacavus_final_proje/util/Constants.java
+++ app/src/main/java/com/hr180011_huseyin_gokhan_karacavus_final_proje/util/Constants.java
@@ -2,7 +2,7 @@ package com.hr180011_huseyin_gokhan_karacavus_final_proje.util;
 
 public class Constants {
 public static final String BASE_URL = "https://raw.githubusercontent.com/GokhanKaracavus/HarryPotterFilmleri/master/";
-public  static String TIKLANAN_FİLM_BASLIGI= "tiklananBurc";
+ public  static String TIKLANAN_FİLM_BASLIGI= "tiklananBurc";
 
 
 }
