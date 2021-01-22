e3a93919cb968fddd338afb2c82e00c0782e9f52
Gökhan Karaçavuş <gokhankaracavus@hotmail.com>
2021-01-22 17:24:22 +0300

    Update Constants.java
    raw link düzeltildi

--- app/src/main/java/com/hr180011_huseyin_gokhan_karacavus_final_proje/util/Constants.java
+++ app/src/main/java/com/hr180011_huseyin_gokhan_karacavus_final_proje/util/Constants.java
@@ -1,7 +1,7 @@
 package com.hr180011_huseyin_gokhan_karacavus_final_proje.util;
 
 public class Constants {
-public static final String BASE_URL = "https://raw.githubusercontent.com/GokhanKaracavus/HarryPotterFilmleri/master/";
+public static final String BASE_URL = "https://raw.githubusercontent.com/GokhanKaracavus/hr180011huseyingokhankaracavus/main/";
  public  static String TIKLANAN_FİLM_BASLIGI= "tiklananBurc";
 
 
